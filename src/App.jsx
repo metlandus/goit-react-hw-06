c4ff8f09c8f7e1cca244d8b7a59ffa0a49132e93
Mete <metlandus@hotmail.com.tr>
2024-12-20 23:50:04 +0300

    All done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
-function App() {
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import ContactForm from './Components/ContactForm/ContactForm';
+import SearchBox from './Components/SearchBox';
+import ContactList from './Components/ContactList';
+import { addContact } from './Redux/contactsSlice';
+import { changeFilter } from './Redux/filtersSlice';
+import styles from './App.module.css';
 
-  return (
-    <>
-    </>
-  )
-}
+const App = () => {
+    const dispatch = useDispatch();
+    const contacts = useSelector((state) => state.contacts.items);
+    const searchValue = useSelector((state) => state.filters.nameFilter);
 
-export default App
+    const handleAddContact = (newContact) => {
+        dispatch(addContact(newContact));
+    };
+
+    const handleSearchChange = (e) => {
+        dispatch(changeFilter(e.target.value));
+    };
+
+    const filteredContacts = contacts.filter((contact) =>
+        contact.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Phonebook</h1>
+            <ContactForm onAddContact={handleAddContact} />
+            <SearchBox inputValue={searchValue} handleChange={handleSearchChange} className={styles.searchBox} />
+            <ContactList contacts={filteredContacts} />
+        </div>
+    );
+};
+
+export default App;
